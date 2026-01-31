6011389382a486a0eee6d4a818140a4b75935ae2
inglesdeuna <direccion@letsinstitute.com.co>
2026-01-30 22:06:12 -0500

    Update editor.php

--- lessons/lessons/activities/flashcards/editor.php
+++ lessons/lessons/activities/flashcards/editor.php
@@ -1,5 +1,5 @@
 <?php
-$file = __DIR__ . "/flashcards.json";
+$file = __DIR__ . "/../../admin/flashcards.json";
 $data = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
 
 /* ===== SAVE ===== */
