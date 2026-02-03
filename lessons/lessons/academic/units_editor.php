f66c148a989ca10d63e4e635a04d787a905653ed
inglesdeuna <direccion@letsinstitute.com.co>
2026-02-03 07:20:24 -0500

    Update units_editor.php

--- lessons/lessons/academic/units_editor.php
+++ lessons/lessons/academic/units_editor.php
@@ -119,6 +119,22 @@ button{
     </div>
   <?php endforeach; ?>
 </div>
+<hr style="margin:40px 0">
+
+<div style="text-align:right">
+  <a href="assignments_editor.php"
+     style="
+       padding:14px 24px;
+       background:#2563eb;
+       color:#fff;
+       text-decoration:none;
+       border-radius:10px;
+       font-weight:700;
+       font-size:16px;
+     ">
+    ➡️ Siguiente: Asignaciones
+  </a>
+</div>
 
 </body>
 </html>
