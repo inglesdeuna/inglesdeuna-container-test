cf2b4e0690c759af69aa75e36ca32395cdca1fd3
inglesdeuna <direccion@letsinstitute.com.co>
2026-02-03 07:12:22 -0500

    Update levels_manager.php

--- lessons/lessons/academic/levels_manager.php
+++ lessons/lessons/academic/levels_manager.php
@@ -130,22 +130,7 @@ select{
        font-size:16px;
      ">
     ➡️ Siguiente: Semestres
-  </a>
-</div>
-<hr style="margin:40px 0">
-
-<div style="text-align:right">
-  <a href="semesters_editor.php"
-     style="
-       padding:14px 24px;
-       background:#2563eb;
-       color:#fff;
-       text-decoration:none;
-       border-radius:10px;
-       font-weight:700;
-       font-size:16px;
-     ">
-    ➡️ Siguiente: Semestres
+ 
   </a>
 </div>
 
