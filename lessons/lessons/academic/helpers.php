f840fb003cf39d9e01eebce8d8277763e5c6c02b
inglesdeuna <direccion@letsinstitute.com.co>
2026-02-06 13:57:09 -0500

    Update helpers.php

--- lessons/lessons/academic/helpers.php
+++ lessons/lessons/academic/helpers.php
@@ -6,23 +6,14 @@ function getUserRole($course, $session) {
     return "editor";
   }
 
-  // DOCENTE
+  // CUALQUIER DOCENTE LOGUEADO = editor
   if (isset($session["teacher_id"])) {
-    if (
-      isset($course["teacher"]["id"]) &&
-      $course["teacher"]["id"] === $session["teacher_id"]
-    ) {
-      return $course["teacher"]["role"] ?? "editor";
-    }
+    return "editor";
   }
 
   // ESTUDIANTE
   if (isset($session["student_id"])) {
-    foreach ($course["students"] ?? [] as $s) {
-      if ($s["id"] === $session["student_id"]) {
-        return $s["role"] ?? "viewer";
-      }
-    }
+    return "viewer";
   }
 
   return "viewer";
