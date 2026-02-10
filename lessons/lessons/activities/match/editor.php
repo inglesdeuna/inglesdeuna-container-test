eaf2bb533aac2fd05faa7bff81cff0437cd1d7d1
inglesdeuna <direccion@letsinstitute.com.co>
2026-02-10 06:15:15 -0500

    Update editor.php

--- lessons/lessons/activities/match/editor.php
+++ lessons/lessons/activities/match/editor.php
@@ -1,8 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . "/../../config/db.php";
 
-$unit = $_GET["unit"] ?? null;
-if(!$unit) die("Unit no especificada");
 
 /* =========================
    UPLOAD DIR
