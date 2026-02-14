8cf2923bba17a3b8484c6bf3abd6e4c09103b58a
inglesdeuna <direccion@letsinstitute.com.co>
2026-02-14 05:48:21 -0500

    Update editor.php

--- lessons/lessons/activities/flashcards/editor.php
+++ lessons/lessons/activities/flashcards/editor.php
@@ -4,6 +4,8 @@ require_once __DIR__."/../../config/init_db.php";
 $type = "flashcards";
 
 require_once __DIR__."/../../core/_activity_editor_template.php";
+$data = $data ?? [];
+
 ?>
 
 <h2>Flashcards Editor</h2>
