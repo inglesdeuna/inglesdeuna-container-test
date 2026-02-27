c9c96aaff9d5a744acf603c59fc84729530a8431
inglesdeuna <direccion@letsinstitute.com.co>
2026-02-27 07:19:13 -0500

    Update db.php

--- lessons/lessons/config/db.php
+++ lessons/lessons/config/db.php
@@ -8,20 +8,18 @@ if (!$databaseUrl) {
 
 $db = parse_url($databaseUrl);
 
-$host = $db['host'];
-$port = $db['port'];
-$user = $db['user'];
-$pass = $db['pass'];
-$dbname = ltrim($db['path'], '/');
+$host     = $db['host'];
+$port     = $db['port'];
+$user     = $db['user'];
+$password = $db['pass'];
+$dbname   = ltrim($db['path'], '/');
 
 try {
     $pdo = new PDO(
         "pgsql:host=$host;port=$port;dbname=$dbname",
         $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]
+        $password,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
