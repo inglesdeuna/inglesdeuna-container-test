fd6f09457021b2005e4677afd9f926fe6bbac0c6
inglesdeuna <direccion@letsinstitute.com.co>
2026-03-05 21:05:27 -0500

    Create ttechnical_assignments.php

---
+++ lessons/lessons/academic/ttechnical_assignments.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['admin_logged']) || $_SESSION['admin_logged'] !== true) {
+    header('Location: ../admin/login.php');
+    exit;
+}
+
+header('Location: assignments_editor.php?program=technical');
+exit;
