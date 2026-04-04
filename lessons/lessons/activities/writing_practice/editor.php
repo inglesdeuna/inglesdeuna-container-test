f0776580ed441b4ea93fe7645ac79936f711a264
InglesDeUna Dev <dev@inglesdeuna.com>
2026-04-04 10:08:23 -0500

    Writing practice 2

--- lessons/lessons/activities/writing_practice/editor.php
+++ lessons/lessons/activities/writing_practice/editor.php
@@ -4,6 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 require_once __DIR__ . '/../../config/db.php';
+require_once __DIR__ . '/../../core/cloudinary_upload.php';
 require_once __DIR__ . '/../../core/_activity_editor_template.php';
 
 // Block student access to editor
