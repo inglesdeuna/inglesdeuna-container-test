e26465dc350c046a2b3028a71d06de762f5797ea
inglesdeuna <direccion@letsinstitute.com.co>
2026-04-29 12:18:33 +0000

    Teacher inscription

--- lessons/lessons/academic/delete_teacher.php
+++ lessons/lessons/academic/delete_teacher.php
@@ -11,15 +11,21 @@ require '../config/db.php';
 $id = $_GET['id'] ?? '';
 
 if ($id !== '') {
-
-    $pdo->prepare("DELETE FROM teachers WHERE id = :id")
-        ->execute(['id'=>$id]);
-
-    $pdo->prepare("DELETE FROM teacher_accounts WHERE teacher_id = :id")
-        ->execute(['id'=>$id]);
-
+    // Primero desvincular estudiantes de este docente
     $pdo->prepare("DELETE FROM student_assignments WHERE teacher_id = :id")
-        ->execute(['id'=>$id]);
+        ->execute(['id' => $id]);
+
+    // Eliminar asignaciones de cursos/unidades del docente
+    $pdo->prepare("DELETE FROM teacher_assignments WHERE teacher_id = :id")
+        ->execute(['id' => $id]);
+
+    // Eliminar cuenta de acceso
+    $pdo->prepare("DELETE FROM teacher_accounts WHERE teacher_id = :id")
+        ->execute(['id' => $id]);
+
+    // Eliminar registro principal
+    $pdo->prepare("DELETE FROM teachers WHERE id = :id")
+        ->execute(['id' => $id]);
 }
 
 header("Location: teacher_groups.php");
