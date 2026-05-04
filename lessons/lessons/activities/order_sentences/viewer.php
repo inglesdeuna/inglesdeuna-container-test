108e758a8062e69dd007f531aceb115550f72771
inglesdeuna <direccion@letsinstitute.com.co>
2026-05-04 17:59:56 -0500

    Implement shuffle list functionality on load
    Add a function to shuffle list items on load.

--- lessons/lessons/activities/order_sentences/viewer.php
+++ lessons/lessons/activities/order_sentences/viewer.php
@@ -1023,6 +1023,21 @@ window.osRestart = function() {
     updateBadges();
 };
 
+(function shuffleListOnLoad() {
+    var items = Array.from(listEl.children);
+
+    for (var i = items.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = items[i];
+        items[i] = items[j];
+        items[j] = temp;
+    }
+
+    items.forEach(function(el) {
+        listEl.appendChild(el);
+    });
+})();
+
 updateBadges();
 
 var TTS_TEXT = <?= json_encode(
